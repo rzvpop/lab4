839e8130c9ebe684d213bae103bd2724f5e49bba
razva <razvan.pop1998@yahoo.com>
2017-11-10 19:58:12 +0200

    lab4

---
+++ repository/Repository.py
@@ -0,0 +1,38 @@
+from domain.exceptions import LibraryException
+
+
+class RepositoryException(LibraryException):
+    pass
+
+class Repository:
+
+    def __init__(self):
+        self.__elems = []
+
+    def add(self, elem):
+        if elem in self.__elems:
+            raise RepositoryException("Existent element!!")
+        self.__elems.append(elem)
+
+    def rem(self, elem):
+        if elem not in self.__elems:
+            raise RepositoryException("Inexistent element!!")
+        self.__elems.remove(elem)
+
+    def upd(self, elem):
+        if elem not in self.__elems:
+            raise RepositoryException("Inexistent element!!")
+        idx = self.__elems.index(elem)
+        self.__elems[idx] = elem
+
+    def find(self, elem):
+        if elem not in self.__elems:
+            raise RepositoryException("Inexistent element!!")
+        idx = self.__elems.index(elem)
+        return self.__elems[idx]
+
+    def size(self):
+        return len(self.__elems)
+
+    def getAll(self):
+        return self.__elems[:]
---
+++ ui/Console.py
@@ -0,0 +1,104 @@
+import re
+
+from domain.exceptions import LibraryException
+
+
+class Console:
+
+    def __init__(self, ctrl):
+        self.__ctrl = ctrl
+
+    def exitApp(self):
+        exit(0)
+
+    def addBook(self):
+        id = input("Id: ")
+        title = input("Title: ")
+        desc = input("Descrpition: ")
+        author = input("Author: ")
+        args = [id, title, desc, author]
+
+        self.__ctrl.addBook(args)
+        print("Book added!")
+
+    def listBooks(self):
+        books = self.__ctrl.getBooks()
+
+        if len(books) == 0:
+            print("No books!")
+            return
+        for x in books:
+            print(x, '\n')
+
+    def removeBook(self, args):
+        self.__ctrl.removeBook(int(args[0]))
+        print("Book removed!")
+
+    def updateBook(self, args):
+        self.__ctrl.updateBook(args)
+        print("Book updated!")
+
+    def addClient(self, args):
+        self.__ctrl.addClient(args)
+        print("Client added!")
+
+    def listClients(self):
+        clients = self.__ctrl.getClients()
+
+        if len(clients) == 0:
+            print("No clients!")
+            return
+
+        for x in clients:
+            print(x, '\n')
+
+    def syntaxDict(self):
+        d = {
+            "add_book": r"^\s*add\s*book\s*$",
+            "list_books": r"^\s*list\s*books\s*$",
+            "remove_book": r"^\s*remove\s*book\s*([0-9]+)\s*$",
+            "update_book": r"^\s*update\s*book\s*([0-9]+)\s*([a-zA-Z0-9]+)\s*([a-zA-Z0-9]+)\s*([a-zA-Z0-9]+)\s*$",
+            "add_client": r"^\s*add\s*client\s*([0-9]+)\s*([a-zA-Z0-9]+)\s*$",
+            "list_clients": r"^\s*list\s*clients\s*$",
+            "exit": r"^\s*exit\s*$"
+        }
+        return d
+
+    def cmdList(self):
+        l = [["exit", self.exitApp, False],
+             ["add_book", self.addBook, True],
+             ["list_books", self.listBooks, False],
+             ["remove_book", self.removeBook, True],
+             ["update_book", self.updateBook, True],
+             ["add_client", self.addClient, True],
+             ["list_clients", self.listClients]]
+        return l
+
+    def run(self):
+        actions = self.cmdList()
+        syntax_dict = self.syntaxDict()
+
+        while True:
+            user_input = input('>>')
+            found = False
+
+            for x, y in syntax_dict.items():
+                matches = re.match(y, user_input)
+
+                if matches:
+                    found = True
+
+                    for i in actions:
+                        if x == i[0]:
+                            if len(matches.groups()) >= 1:
+                                try:
+                                    i[1](matches.groups())
+                                except LibraryException as ex:
+                                    print(ex)
+                            else:
+                                try:
+                                    i[1]()
+                                except LibraryException as ex:
+                                    print(ex)
+            if not found:
+                print("Invalid command syntax!")
